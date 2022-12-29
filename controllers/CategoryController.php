0229aa2ed09b46b13f4c16ebaf13ccb471094a2d
PouyaniArmin <pouyaniarmin@gmial.com>
2022-12-29 18:32:32 +0330

    add funtion create(insert to table)

--- controllers/CategoryController.php
+++ controllers/CategoryController.php
@@ -3,31 +3,35 @@
 namespace Controllers;
 
 use Core\Authentication;
-use Core\Controller;
 use Core\Database;
 use Core\Model;
 use Core\Request;
 
 class CategoryController extends Authentication
 {
+
     public function index(Request $request)
     {
-
-        $model = new Model();
-        $db = new Database();
-        if ($request->post()) {
-            $body = $request->body();
-            $model->set_data($body);
-            if ($model->validation()) {
-                $this->setLayout('dashboard');
-                //add insert to database
-                $db->insert($body['name']);
-                $data = ['success' => "add category"];
-                return $this->view('category', $data);
-            }
-            $this->setLayout('dashboard');
-            return $this->view('category', $model->errors);
-        }
         return $this->authorization($request, $request->body(), 'category');
+    }
+    
+    public function create(Request $request)
+    {
+        if ($request->post()) {
+            $model = new Model();
+            $db = new Database();
+            if ($request->post()) {
+                $body = $request->body();
+                $model->set_data($body);
+                if ($model->validation()) {
+                    $this->setLayout('dashboard');
+                    $db->insert($body['name']);
+                    $data = ['success' => "add category"];
+                    return $this->view('category', $data);
+                }
+                $this->setLayout('dashboard');
+                return $this->view('category', $model->errors);
+            }
+        }
     }
 }
