a8ab610a9583c924bf7438895c6cde1ad1ea523a
PouyaniArmin <pouyaniarmin@gmial.com>
2022-12-29 11:37:40 +0330

    insert db(not fix)

--- controllers/CategoryController.php
+++ controllers/CategoryController.php
@@ -1,11 +1,29 @@
 <?php
 namespace Controllers;
 
+use Core\Authentication;
 use Core\Controller;
+use Core\Database;
+use Core\Model;
+use Core\Request;
 
-class CategoryController extends Controller{
-    public function index(){
-        $this->setLayout('dashboard');
-        return $this->view('category');
+class CategoryController extends Authentication{
+    public function index(Request $request){
+        
+        $model=new Model();
+        $db=new Database();
+        if ($request->post()) {
+            $body=$request->body();
+            $model->set_data($body);
+            if ($model->validation()) {
+                $data=['success'=>"add category"];
+                $this->setLayout('dashboard');
+                //add insert to database
+                return $this->view('category',$data);
+            }
+         $this->setLayout('dashboard');
+         return $this->view('category',$model->errors);   
+        }
+        return $this->authorization($request,$request->body(),'category');
     }
 }
