15e134596683e322f786e30f6625c753a424947b
PouyaniArmin <pouyaniarmin@gmial.com>
2022-12-29 12:05:59 +0330

    add insert to table

--- controllers/CategoryController.php
+++ controllers/CategoryController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Controllers;
 
 use Core\Authentication;
@@ -7,23 +8,26 @@ use Core\Database;
 use Core\Model;
 use Core\Request;
 
-class CategoryController extends Authentication{
-    public function index(Request $request){
-        
-        $model=new Model();
-        $db=new Database();
+class CategoryController extends Authentication
+{
+    public function index(Request $request)
+    {
+
+        $model = new Model();
+        $db = new Database();
         if ($request->post()) {
-            $body=$request->body();
+            $body = $request->body();
             $model->set_data($body);
             if ($model->validation()) {
-                $data=['success'=>"add category"];
                 $this->setLayout('dashboard');
                 //add insert to database
-                return $this->view('category',$data);
+                $db->insert($body['name']);
+                $data = ['success' => "add category"];
+                return $this->view('category', $data);
             }
-         $this->setLayout('dashboard');
-         return $this->view('category',$model->errors);   
+            $this->setLayout('dashboard');
+            return $this->view('category', $model->errors);
         }
-        return $this->authorization($request,$request->body(),'category');
+        return $this->authorization($request, $request->body(), 'category');
     }
 }
