448cef26cde10ecbe43e0381f0a211e00bd49c2d
PouyaniArmin <pouyaniarmin@gmial.com>
2022-12-31 20:14:07 +0330

    dashboard controller

--- controllers/DashboardController.php
+++ controllers/DashboardController.php
@@ -10,62 +10,11 @@ use Core\Request;
 use PDO;
 use PDOException;
 
-class DashboardController extends Controller
+class DashboardController extends Authentication
 {
     public function index(Request $request)
     {
-        session_start();
-        if ($_SESSION['login'] === true) {
-            $this->setLayout('dashboard');
-            return $this->view('dashboard');
-        }
-        $db = new Database();
-        $model = new Model;
-        if ($request->post()) {
-            $model->set_data($request->body());
-            if ($model->validation()) {
-                $body = $request->body();
-                $email = $body['email'];
-                $password = $body['password'];
-                if ($db->login($email, $password)) {
-                    $this->setLayout('dashboard');
-                    return $this->view('dashboard');
-                }
-                return $this->view('sign', $db->errors);
-            }
-            return $this->view('sign', $model->errors);
-        }
-        return $this->view('sign');
+        return $this->authorization($request,$request->body(),'dashboard');
     }
 
-    public function dashboardCheck(Request $request){        
-        session_start();
-        if ($_SESSION['login'] === true) {
-            $this->setLayout('dashboard');
-            //view session
-            return $this->view('dashboard');
-        }
-        $db = new Database();
-        $model = new Model;
-        if ($request->post()) {
-            $model->set_data($request->body());
-            //nacessary validate for form
-            if ($model->validation()) {
-                $body = $request->body();
-                $email = $body['email'];
-                $password = $body['password'];
-                if ($db->login($email, $password)) {
-                    $this->setLayout('dashboard');
-                    //view controller
-                    return $this->view('dashboard');
-                }
-                //view controller
-                return $this->view('sign', $db->errors);
-            }
-            //view controller
-            return $this->view('sign', $model->errors);
-        }
-        //view
-        return $this->view('sign');       
-    }
 }
