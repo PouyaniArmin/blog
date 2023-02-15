9791b70e0cbfbeaf055ae2a8167f26f6d5117b47
PouyaniArmin <pouyaniarmin@gmail.com>
2023-02-15 16:02:21 +0330

    view

---
+++ views/home.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+    <h2>Welcome <?php echo $username;?> to my website </h2>
+   <div style="padding: 50px;">
+   <form action="">
+      <input type="text" name="name" placeholder="name">
+      <br>
+      <br>
+      <input type="email" name="email" placeholder="email">
+      <br>
+      <br>
+      <button type="submit">Save</button>
+    </form>
+   </div>
+  </body>
+</html>
