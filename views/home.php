c6a752ca1e0ffca489df7e5b8e9aa60152411c88
PouyaniArmin <pouyaniarmin@gmail.com>
2023-02-21 11:06:55 +0330

    view

--- views/home.php
+++ views/home.php
@@ -4,20 +4,13 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
   </head>
   <body>
+    <div class="p-4">
     <h1>Hello, world!</h1>
-    <h2>Welcome <?php echo $username;?> to my website </h2>
-   <div style="padding: 50px;">
-   <form action="">
-      <input type="text" name="name" placeholder="name">
-      <br>
-      <br>
-      <input type="email" name="email" placeholder="email">
-      <br>
-      <br>
-      <button type="submit">Save</button>
-    </form>
-   </div>
+    <a href="" class="btn btn-primary">home</a>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
   </body>
 </html>
---
+++ views/register.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+<body>
+    <section class="p-4">
+        <div class="container pt-4">
+        <form method="post" class="w-50">
+        <div class="mb-3">
+                <label for="exampleInputUsername" class="form-label">Username</label>
+                <input type="text" name="username" class="form-control" id="exampleInputUsername" aria-describedby="emailHelp">
+            </div>
+            <div class="text-danger"><p> <?php echo $username; ?></p></div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                <input type="text" name="email" class="form-control">
+            </div>
+
+            <div class="text-danger"><p> <?php echo $email; ?></p></div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Password</label>
+                <input type="password" name="password" class="form-control" id="exampleInputPassword1">
+            </div>
+            <div class="text-danger"><p> <?php echo $password; ?></p></div>
+            <div class="mb-3">
+                <label for="exampleInputConfirmPassword1" class="form-label">Confirm Password</label>
+                <input type="password" name="confirmPassword" class="form-control" id="exampleInputConfirmPassword1">
+            </div>
+            <div class="text-danger"><p> <?php echo $confirmPassword; ?></p></div>
+            <button type="submit" class="btn btn-primary">Register</button>
+        </form>
+        </div>
+        </div>
+    </section>
+</body>
+</html>
