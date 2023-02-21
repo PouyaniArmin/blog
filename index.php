990ee977b1f0909a42122b32445b3718c11d35c5
PouyaniArmin <pouyaniarmin@gmail.com>
2023-02-21 11:07:08 +0330

    index

--- index.php
+++ index.php
@@ -1,12 +1,21 @@
 <?php
 
+use Controllers\ContactController;
 use Controllers\HomeControllers;
+use Controllers\UserController;
 use Core\Application;
+use Core\Router;
 use Dotenv\Dotenv;
 
 require_once realpath(__DIR__.'/vendor/autoload.php');
 
 
 $app=new Application(__DIR__);
-$app->get('/','home');
+Router::get('/','home');
+Router::get('/contact',[ContactController::class,'index']);
+Router::get('/about','about');
+Router::get('/login',[UserController::class,'login']);
+Router::post('/login',[UserController::class,'login']);
+Router::get('/register',[UserController::class,'register']);
+Router::post('/register',[UserController::class,'register']);
 $app->run();
