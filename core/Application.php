e58b857c82d7d612e9264d8c87720f750a67eb20
PouyaniArmin <pouyaniarmin@gmail.com>
2023-03-08 15:47:13 +0330

    Add Session object

--- core/Application.php
+++ core/Application.php
@@ -6,6 +6,7 @@ class Application{
   public Request $request;
   public Router $router;
   public Controller $controller;
+  public Session $session;
   public static Application $app; 
   public function __construct($path)
   {
@@ -13,6 +14,7 @@ class Application{
     self::$ROOT_PATH=$path;
     $this->request=new Request;
     $this->router=new Router($this->request);
+    $this->session=new Session;
   }
   public function getController():Controller{
     return $this->controller;
