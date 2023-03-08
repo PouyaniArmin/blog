336787730879c244b44717c721b63903c4fa5aa5
PouyaniArmin <pouyaniarmin@gmail.com>
2023-03-08 15:47:40 +0330

    controller class

--- core/Controller.php
+++ core/Controller.php
@@ -1,9 +1,20 @@
-<?php 
+<?php
 
 namespace Core;
 
-class Controller{
-    public function view($view,$params=[]){
-       return Application::$app->router->renderView($view,$params);
+session_start();
+class Controller extends Database
+{
+    public string $auth;
+    public function autocation(string $auth){
+        $this->auth=$auth;
+    }
+    public function view($view, $params = [])
+    {
+        return Application::$app->router->renderView($view, $params);    
+    }
+
+    public function redirecctTo($view){
+        header("Location:$view");
     }
 }
