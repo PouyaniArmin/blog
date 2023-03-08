58b686d20c172983364239a8bc4d997acfaa79bf
PouyaniArmin <pouyaniarmin@gmail.com>
2023-03-08 15:46:39 +0330

    user controller register and login

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -1,36 +1,67 @@
 <?php
 
 namespace Controllers;
+session_start();
 
-use Core\Controller;
+use Core\Authentication;
 use Core\Request;
 use Models\LoginModel;
 use Models\RegisterModel;
 
-class UserController extends Controller
+
+class UserController extends Authentication
 {
+
+    public function __construct()
+    {
+    }
+    public $page;
+
     public function login(Request $request)
     {
+        $loinModel = new LoginModel();
         if ($request->isPost()) {
-            $login = new LoginModel($request->body());
-            if ($login->validate()) {
+            $loinModel->loadData($request->body());
+            if ($loinModel->validate()) {
+                $body = $request->body();
+                if ($loinModel->logined($body['email'], $body['password'])) {
+                    $this->set_after_login_view();
+                }   
             }
+            return $this->view('login', $loinModel->errors);
         }
         return $this->view('login');
     }
 
+
+
     public function register(Request $request)
     {
-        if ($request->isPost()) {
-            $register = new RegisterModel($request->body());
-            if ($register->validate()) {
-             echo "test";
-            }
 
-            return $this->view('register',$register->errors);
-            
-        
+        if (isset($_SESSION['login'])) {
+            echo "Please logout account atfer register to weblog";
+            header('Location:/dashboard');
+            exit;
+           }
+
+        $register = new RegisterModel($request->body());
+        if ($request->isPost()) {
+            $register->loadData($request->body());
+            if ($register->validate()) {
+                echo "success";
+            }
+            return $this->view('register', $register->errors);
         }
         return $this->view('register');
     }
+
+    public function logout(){
+        session_destroy();
+        header("Location:/");
+    }
+
+
+
 }
+
+?>
