2264e561ef76d4282c7c1357c4099f0917dc064d
PouyaniArmin <pouyaniarmin@gmail.com>
2023-03-16 18:16:36 +0330

    view

---
+++ views/newProduct.php
@@ -0,0 +1,21 @@
+<div class="sahel-font">
+<h1>افزودن محصول</h1>
+
+
+<form action="">
+<div class="mb-3">
+  <label for="formGroupExampleInput" class="form-label">نام محصول</label>
+  <input type="text" class="form-control" name="name" id="formGroupExampleInput" placeholder="Example input placeholder">
+</div>
+<div class="mb-3">
+  <label for="formGroupExampleInput" class="form-label">توضیحات</label>
+    <textarea name="body" class="create-textarea" id="" cols="105" rows="8"></textarea>
+</div>
+
+<div class="mb-3">
+  <label for="formGroupExampleInput" class="form-label">افزودن تصویر</label>
+  <input type="file" class="form-control" name="name" id="formGroupExampleInput" placeholder="Example input placeholder">
+</div>
+<button class="btn btn-outline-success" type="submit">ثبت</button>
+</form>
+</div>
---
+++ views/shop.php
@@ -0,0 +1,61 @@
+<div class="py-4">
+    <div class="container store sahel-font">
+        <div>
+            <h4 class="pb-4">لیست محصولات</h4>
+        </div>
+        <div>
+            <div class="row">
+                <div class="col-3">
+                    <h5>افزودن محصول جدید</h5>
+                </div>
+                <div class="col-1">
+                    <a href="/shop/create"><i><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-plus-square-fill" viewBox="0 0 16 16">
+                                <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3a.5.5 0 0 1 1 0z" />
+                            </svg></i></a>
+                </div>
+            </div>
+        </div>
+        <ul class="list-group list-group-flush">
+            <?php for ($i = 1; $i < 10; $i++) { ?>
+                <li class="list-group-item pt-3">
+                    <div class="row">
+                        <div class="col-8">
+                            <input class="form-check-input me-1" type="checkbox" value="" id="firstCheckbox">
+                            <label class="form-check-label me-4" for="firstCheckbox">نام محصول <?php echo $i ?></label>
+                        </div>
+                        <div class="col-4 text-start">
+                            <a class="text-end" href="">ویرایش <i> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
+                                        <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z" />
+                                    </svg></i></a>
+                            <a class="text-end text-danger" href="">حذف <i><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
+                                        <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z" />
+                                    </svg></i></a>
+                        </div>
+                    </div>
+                </li>
+            <?php }; ?>
+            <div class="row py-2">
+                <div class="col-6">
+                    <nav aria-label="Page navigation example">
+                        <ul class="pagination">
+                            <li class="page-item disabled">
+                                <a class="page-link">Previous</a>
+                            </li>
+                            <li class="page-item"><a class="page-link" href="#">1</a></li>
+                            <li class="page-item"><a class="page-link" href="#">2</a></li>
+                            <li class="page-item"><a class="page-link" href="#">3</a></li>
+                            <li class="page-item">
+                                <a class="page-link" href="#">Next</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+                <div class="col-6 text-start">
+                    <a class="text-danger me-4" href="">Delete All</a>
+                    <a class="text-danger ms-4" href="">Delete marked</a>
+                </div>
+            </div>
+        </ul>
+    </div>
+
+</div>
